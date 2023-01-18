4bc1269d5dce2ac84f4946018685d913b24b5823
prajwal <prajwal1majjigi@gmail.com>
2023-01-18 18:42:11 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
+
+
 const App = () => {
+  
+  const relatives=[{name:'Ramesh',key:'relativeListItem1'},{name:'Rahul',key:'relativeListItem2'},{name:'Suresh',key:'relativeListItem3'},{name:'Ravi',key:'relativeListItem4'}]
+    
+  const items=relatives.map((relative)=>
+  <li key={relative.key}>{relative.name}</li>
+  );
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      <h1>My Relatives</h1>
+      <li>{items}</li>
+
+
     </div>
   )
 }
